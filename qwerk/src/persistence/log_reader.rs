15fefe7fd86d3acda688deda1019a6f6cedb93da
Yuta Imazu <yuta.imazu@gmail.com>
2023-12-24 17:16:09 +0900

    Reject corrupted log tail

--- qwerk/src/persistence/log_reader.rs
+++ qwerk/src/persistence/log_reader.rs
@@ -1,14 +1,23 @@
-use crate::{bytes_ext::ReadBytesExt, small_bytes::SmallBytes, tid::Tid};
-use std::{fs::File, io::BufReader, path::Path};
+use crate::{bytes_ext::ReadBytesExt, small_bytes::SmallBytes, tid::Tid, Error, Result};
+use std::{
+    cmp::Ordering,
+    fs::File,
+    io::{BufReader, Read},
+    path::Path,
+};
 
 pub struct LogReader {
-    file: BufReader<File>,
+    file: ReadBytesCounter<BufReader<File>>,
+    file_len: u64,
 }
 
 impl LogReader {
     pub fn new(path: &Path) -> std::io::Result<Self> {
+        let file = File::open(path)?;
+        let file_len = file.metadata()?.len();
         Ok(Self {
-            file: BufReader::new(File::open(path)?),
+            file: ReadBytesCounter::new(BufReader::new(file)),
+            file_len,
         })
     }
 
@@ -29,16 +38,21 @@ impl LogReader {
 }
 
 impl Iterator for LogReader {
-    type Item = std::io::Result<LogTransaction>;
+    type Item = Result<LogTransaction>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        match self.read_txn() {
-            Ok(txn) => Some(Ok(txn)),
-            Err(e) if e.kind() == std::io::ErrorKind::UnexpectedEof => {
-                // TODO: optionally give up recovery if logs are corrupted
-                None
+        let num_bytes_read = self.file.num_bytes_read() as u64;
+        match num_bytes_read.cmp(&self.file_len) {
+            Ordering::Less => Some(self.read_txn().map_err(Into::into)),
+            Ordering::Equal => {
+                // Make sure we have reached EOF.
+                match self.file.read(&mut [0; 1]) {
+                    Ok(0) => None,
+                    Ok(_) => Some(Err(Error::DatabaseCorrupted)),
+                    Err(e) => Some(Err(e.into())),
+                }
             }
-            Err(e) => Some(Err(e)),
+            Ordering::Greater => Some(Err(Error::DatabaseCorrupted)),
         }
     }
 }
@@ -51,4 +65,36 @@ pub struct LogEntry {
 pub struct LogTransaction {
     pub tid: Tid,
     pub entries: Vec<LogEntry>,
+}
+
+struct ReadBytesCounter<R> {
+    inner: R,
+    num_bytes_read: usize,
+}
+
+impl<R: Read> ReadBytesCounter<R> {
+    fn new(inner: R) -> Self {
+        Self {
+            inner,
+            num_bytes_read: 0,
+        }
+    }
+
+    fn num_bytes_read(&self) -> usize {
+        self.num_bytes_read
+    }
+}
+
+impl<R: Read> Read for ReadBytesCounter<R> {
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        let num_bytes_read = self.inner.read(buf)?;
+        self.num_bytes_read += num_bytes_read;
+        Ok(num_bytes_read)
+    }
+
+    fn read_exact(&mut self, buf: &mut [u8]) -> std::io::Result<()> {
+        self.inner.read_exact(buf)?;
+        self.num_bytes_read += buf.len();
+        Ok(())
+    }
 }
