5e96654c196806ea0fbbbc436eb8b6ced98c8739
Yuta Imazu <yuta.imazu@gmail.com>
2024-01-03 17:25:11 +0900

    Validate checkpoint and log files based on FileId

--- qwerk/src/persistence/log_reader.rs
+++ qwerk/src/persistence/log_reader.rs
@@ -1,4 +1,5 @@
-use crate::{bytes_ext::ReadBytesExt, small_bytes::SmallBytes, tid::Tid, Error, Result};
+use super::file_id::LogFileId;
+use crate::{bytes_ext::ReadBytesExt, small_bytes::SmallBytes, tid::Tid, Epoch, Error, Result};
 use std::{
     cmp::Ordering,
     fs::File,
@@ -9,41 +10,55 @@ use std::{
 pub struct LogReader {
     file: ReadBytesCounter<BufReader<File>>,
     file_len: u64,
+    max_epoch: Option<Epoch>,
+    prev_tid: Tid,
 }
 
 impl LogReader {
-    pub fn new(path: &Path) -> std::io::Result<Self> {
-        let file = File::open(path)?;
+    pub fn new(dir: &Path, file_id: &LogFileId) -> std::io::Result<Self> {
+        let file = File::open(dir.join(file_id.file_name()))?;
         let file_len = file.metadata()?.len();
         Ok(Self {
             file: ReadBytesCounter::new(BufReader::new(file)),
             file_len,
+            max_epoch: file_id.max_epoch(),
+            prev_tid: Tid(0),
         })
     }
 
-    fn read_txn(&mut self) -> std::io::Result<LogTransaction> {
+    fn read_entry(&mut self) -> Result<LogEntry> {
         let tid = Tid(self.file.read_u64()?);
+        let epoch = tid.epoch();
+        if self.max_epoch.map_or(false, |max_epoch| epoch > max_epoch)
+            || tid <= self.prev_tid
+            || epoch < self.prev_tid.epoch()
+        {
+            return Err(Error::DatabaseCorrupted);
+        }
+        self.prev_tid = tid;
+
         let num_records = self.file.read_u64()? as usize;
-        let mut entries = Vec::with_capacity(num_records);
+        let mut records = Vec::with_capacity(num_records);
         for _ in 0..num_records {
             let key = self.file.read_bytes()?;
             let value = self.file.read_maybe_bytes()?.map(Into::into);
-            entries.push(LogEntry {
+            records.push(LogRecord {
                 key: key.into(),
                 value,
             });
         }
-        Ok(LogTransaction { tid, entries })
+
+        Ok(LogEntry { tid, records })
     }
 }
 
 impl Iterator for LogReader {
-    type Item = Result<LogTransaction>;
+    type Item = Result<LogEntry>;
 
     fn next(&mut self) -> Option<Self::Item> {
         let num_bytes_read = self.file.num_bytes_read() as u64;
         match num_bytes_read.cmp(&self.file_len) {
-            Ordering::Less => Some(self.read_txn().map_err(Into::into)),
+            Ordering::Less => Some(self.read_entry().map_err(Into::into)),
             Ordering::Equal => {
                 // Make sure we have reached EOF.
                 match self.file.read(&mut [0; 1]) {
@@ -57,14 +72,14 @@ impl Iterator for LogReader {
     }
 }
 
-pub struct LogEntry {
+pub struct LogRecord {
     pub key: SmallBytes,
     pub value: Option<Box<[u8]>>,
 }
 
-pub struct LogTransaction {
+pub struct LogEntry {
     pub tid: Tid,
-    pub entries: Vec<LogEntry>,
+    pub records: Vec<LogRecord>,
 }
 
 struct ReadBytesCounter<R> {
